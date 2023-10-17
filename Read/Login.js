01368a940bab1206a959c190a4ab3082c8577df4
JPruezkiez1 <jpruezkiez@icloud.com>
2023-10-16 22:22:52 -0500

    Update Login.js

--- Read/Login.js
+++ Read/Login.js
@@ -4,27 +4,31 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     const { username, password } = req.body;
+
     db.query(
         'SELECT * FROM customers WHERE username = ? AND password = ?',
         [username, password],
         (err, results) => {
             if (err) {
                 console.error('Error executing SQL query:', err);
-                return res.status(500).end();
+                return res.status(500).json({ error: 'Internal Server Error' });
             }
             if (results.length === 0) {
-                return res.status(401).end();
+                return res.status(401).json({ error: 'Invalid credentials' });
             }
+
             const customer = results[0];
             const loggedInUser = {
                 username: customer.username,
                 firstName: customer.first_name,
                 lastName: customer.last_name,
                 status: customer.active,
-                image: customer.image
+                image: customer.image // You'll need to specify the image URL or path here.
             };
+
             return res.status(200).json({ loggedInUser });
         }
     );
 });
+
 module.exports = router;
