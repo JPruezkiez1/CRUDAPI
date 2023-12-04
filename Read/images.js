55b629d2ace54434859b7ca24141ec1d9b0de865
JPruezkiez1 <jpruezkiez@icloud.com>
2023-12-04 14:32:25 -0500

    fixed response

--- Read/images.js
+++ Read/images.js
@@ -25,7 +25,7 @@ router.get('/:id', (req, res) => {
             }
 
             const fileUrl = baseURL + results[0].file;
-            return res.status(200).json({ id: results[0].id, uploadname: results[0].upload, file: fileUrl, Owner: results[0].username, OwnerID: results[0].customerId });
+            return res.status(200).json({ id: results[0].id, uploadname: results[0].upload, File_link: fileUrl, File: results[0].file, Owner: results[0].username, OwnerID: results[0].customerId });
         });
     });
 });
@@ -49,7 +49,8 @@ router.get('/', (req, res) => {
                 return {
                     id: result.id,
                     uploadname: result.upload,
-                    file: baseURL + result.file,
+                    File_Link: baseURL + result.file,
+                    file: result.file,
                     Owner: result.username,
                     OwnerId: result.customerId,
                 };
@@ -83,7 +84,7 @@ router.get('/uploadname/:uploadname', (req, res) => {
 
             const files = results.map(result => {
                 const fileUrl = baseURL + result.file;
-                return { id: result.id, uploadname: result.upload, file: fileUrl, Owner: result.username, OwnerId: result.customerId };
+                return { id: result.id, uploadname: result.upload, File_link: fileUrl, File: result.file, Owner: result.username, OwnerId: result.customerId };
             });
 
             return res.status(200).json(files);
