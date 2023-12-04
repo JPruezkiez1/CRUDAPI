6f760fd02da176a3e51f98c1ff42b90ca65a08a9
JPruezkiez1 <jpruezkiez@icloud.com>
2023-12-04 14:45:47 -0500

    Update images.js

--- Read/images.js
+++ Read/images.js
@@ -25,7 +25,7 @@ router.get('/:id', (req, res) => {
             }
 
             const fileUrl = baseURL + results[0].file;
-            return res.status(200).json({ id: results[0].id, upload: results[0].upload, File_link: fileUrl, File: results[0].file, Owner: results[0].username, OwnerID: results[0].customerId });
+            return res.status(200).json({ id: results[0].id, upload: results[0].upload, File_link: fileUrl, File: results[0].file, Owner: results[0].username, OwnerID: results[0].customerId, mbsize: results[0].sizeInMB, kbsize: results[0].sizeInKB, });
         });
     });
 });
@@ -53,6 +53,8 @@ router.get('/', (req, res) => {
                     file: result.file,
                     Owner: result.username,
                     OwnerId: result.customerId,
+                    mbsize: result.sizeInMB,
+                    kbsize: result.sizeInKB,
                 };
             });
 
@@ -84,7 +86,7 @@ router.get('/uploadname/:uploadname', (req, res) => {
 
             const files = results.map(result => {
                 const fileUrl = baseURL + result.file;
-                return { id: result.id, upload: result.upload, File_link: fileUrl, File: result.file, Owner: result.username, OwnerId: result.customerId };
+                return { id: result.id, upload: result.upload, File_link: fileUrl, mbsize: result.sizeInMB, kbsize: result.sizeInKB, File: result.file, Owner: result.username, OwnerId: result.customerId };
             });
 
             return res.status(200).json(files);
