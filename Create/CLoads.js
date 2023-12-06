ed07cebabdcdb86016b7e87590c80b4f945bc218
JPruezkiez1 <jpruezkiez@icloud.com>
2023-12-05 20:46:28 -0500

    Update CLoads.js

--- Create/CLoads.js
+++ Create/CLoads.js
@@ -7,9 +7,17 @@ router.post('/', (req, res) => {
     if (!load || !events || !Array.isArray(events) || events.length === 0) {
         return res.status(400).send('Invalid request data');
     }
+    if (!load.Payment || !load.Driver || !load.Rate_Confirmation || !load.Truck_VIN || !load.Dispatcher) {
+        return res.status(400).send('Missing required load data');
+    }
     if (events[0].EventType !== 'PICKUP' || events[events.length - 1].EventType !== 'DROP-OFF') {
         return res.status(400).send('The first event should be a pickup and the last event should be a drop-off');
     }
+    events.forEach((event) => {
+        if (!event.State || !event.Country || !event.City || !event.Status || !event.POD) {
+            return res.status(400).send('Missing required event data');
+        }
+    });
     db.query('INSERT INTO Loads SET ?', load, (err, result) => {
         if (err) throw err;
         const loadID = result.insertId;
