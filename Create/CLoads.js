4767caa6092c1101448be254156c314b11be16ec
JPruezkiez1 <jpruezkiez@icloud.com>
2023-12-08 20:00:59 -0500

    SHITCHANGE

--- Create/CLoads.js
+++ Create/CLoads.js
@@ -10,14 +10,21 @@ router.post('/', (req, res) => {
     if (!load.Payment || !load.Driver || !load.Rate_Confirmation || !load.Truck_VIN || !load.Dispatcher) {
         return res.status(400).send('Missing required load data');
     }
+
+    // Sort the events array based on the CallOrder property
+    events.sort((a, b) => a.CallOrder - b.CallOrder);
+
+    // Check if the first event is a PICKUP and the last event is a DROP-OFF
     if (events[0].EventType !== 'PICKUP' || events[events.length - 1].EventType !== 'DROP-OFF') {
         return res.status(400).send('The first event should be a pickup and the last event should be a drop-off');
     }
+
     events.forEach((event) => {
         if (!event.State || !event.Country || !event.City || !event.Status || !event.POD) {
             return res.status(400).send('Missing required event data');
         }
     });
+
     db.query('INSERT INTO Loads SET ?', load, (err, result) => {
         if (err) throw err;
         const loadID = result.insertId;
@@ -32,3 +39,4 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
