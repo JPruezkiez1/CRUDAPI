84e092ede995f0db6333d702b2d978b402580070
JPruezkiez1 <jpruezkiez@icloud.com>
2023-12-12 08:08:01 -0500

    Update CLoads.js

--- Create/CLoads.js
+++ Create/CLoads.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../db.js');
 const router = express.Router();
 
-router.post('/', async (req, res) => {  // Make the callback async
+router.post('/', async (req, res) => {
     const { load, events } = req.body;
     if (!load || !events || !Array.isArray(events) || events.length === 0) {
         return res.status(400).send('Invalid request data');
@@ -11,28 +11,22 @@ router.post('/', async (req, res) => {  // Make the callback async
         return res.status(400).send('Missing required load data');
     }
 
-    // Sort the events array based on the CallOrder property
     events.sort((a, b) => a.CallOrder - b.CallOrder);
-
-    // Check if the first event is a PICKUP and the last event is a DROP-OFF
     if (events[0].EventType !== 'PICKUP' || events[events.length - 1].EventType !== 'DROP-OFF') {
         return res.status(400).send('The first event should be a pickup and the last event should be a drop-off');
     }
 
     events.forEach((event) => {
-        if (!event.State || !event.Country || !event.City || !event.Status || !event.POD) {
+        if (!event.State || !event.Country || !event.City || !event.Status) {
             return res.status(400).send('Missing required event data');
         }
     });
-
     db.query('INSERT INTO Loads SET ?', load, (err, result) => {
         if (err) throw err;
         const loadID = result.insertId;
         events.forEach(async (event) => {
             event.LoadID = loadID;
-
-            // Geocode the event address using Google Maps Geocoding API
-            const fetch = await import('node-fetch');  // Use dynamic import() instead of require()
+            const fetch = await import('node-fetch');
             const response = await fetch.default(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(event.Address)}&key=AIzaSyCztUKCZ4mi0VCHzUAaAtSY3aXMi1sqRYg`);
             const data = await response.json();
             if (data.results && data.results.length > 0) {
@@ -40,7 +34,6 @@ router.post('/', async (req, res) => {  // Make the callback async
                 event.lat = location.lat;
                 event.lng = location.lng;
             }
-
             db.query('INSERT INTO Events SET ?', event, (err, result) => {
                 if (err) throw err;
             });
