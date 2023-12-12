153cfecdcfc0590b1caf450771f069db227345a6
JPruezkiez1 <jpruezkiez@icloud.com>
2023-12-12 11:18:13 -0500

    fix

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const port = 8080;
 const app = express();
-const usersRoute = require('./Read/users');
-const ordersRoute = require('./Read/orders');
+
+//Orders
 const productsRoute = require('./Read/products.js');
-const addusersRoute = require('./Create/users');
+const ordersRoute = require('./Read/orders');
 const addordersRoute = require('./Create/Orders');
 const deleteorderRoute = require('./Delete/delete-order');
+// Images Links
 const imagesRoute = require('./Read/images.js');
+/// users
+const usersRoute = require('./Read/users');
 const login = require('./Read/Login.js');
 const userdelete = require('./Delete/DeleteUsers.js')
 const updateusers = require('./Update/UpdateUsers.js')
+const addusersRoute = require('./Create/users');
+/// Load things
 const cloads = require('./Create/CLoads.js');
 const gloads = require('./Read/GetLoads.js')
+
 app.use(cors());
 app.use(express.json());
 
